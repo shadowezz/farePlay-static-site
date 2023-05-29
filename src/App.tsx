9281557793eb8311655239e1d2e6f7c2b39a805a
shadowezz <wenjie.cao@hotmail.com>
2023-05-30 01:30:04 +0800

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="spinner-container">
+          <div className="loading-spinner">
+          </div>
+        </div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Please wait while we log you in.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
